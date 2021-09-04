427a49b90e1d0f12b4f9eec3d87726a0f688cbf6
Ateo101 <nikmail242@gmail.com>
2021-09-04 21:20:08 +0900

    Task #3 - Done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
     totalUsers: any // need to fix any
 }
 
@@ -13,13 +13,13 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = name ? s.filled : s.error // need to fix with (?:)
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
+            <input value={name} onChange={setNameCallback} className={inputClass} placeholder="Type your name..."/>
+            <span className={s.errorMsg}>{error}</span>
+            <button onClick={addUser} className={s.btn}>add</button>
             <span>{totalUsers}</span>
         </div>
     )
